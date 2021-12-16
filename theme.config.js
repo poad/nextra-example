b166ba6f4ff863e66e935a7122a40a6a05ace6af
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-12-17 08:49:25 +0900

    Initial commit

---
+++ theme.config.js
@@ -0,0 +1,24 @@
+export default {
+    repository: 'https://github.com/shuding/nextra', // project repo
+    docsRepository: 'https://github.com/shuding/nextra', // docs repo
+    branch: 'master', // branch of docs
+    path: '/', // path of docs
+    titleSuffix: ' – Nextra',
+    nextLinks: true,
+    prevLinks: true,
+    search: true,
+    customSearch: null, // customizable, you can use algolia for example
+    darkMode: true,
+    footer: true,
+    footerText: `MIT ${new Date().getFullYear()} © Kenji Saito.`,
+    footerEditOnGitHubLink: true, // will link to the docs repo
+    logo: <>
+      <svg>...</svg>
+      <span>Next.js Static Site Generator</span>
+    </>,
+    head: <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next docs builder" />
+      <meta name="og:title" content="Nextra: the next docs builder" />
+    </>
+  }
