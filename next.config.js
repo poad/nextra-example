fb171f4f9a447af970c119361ace9999abb60372
Kenji Saito <ken-yo@mbr.nifty.com>
2022-05-16 00:29:41 +0900

    Add the analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
-module.exports = withNextra({
+/** @type {import('next').NextConfig} */
+const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(withNextra({
     webpack5: true,
-    reactStrictMode: false,
+    reactStrictMode: true,
     esmExternals: true,
-})
+    swcLoader: true,
+    swcMinify: true,
+    esmExternals: true,
+    experimental: {
+        modern: true,
+    }
+}
+));
